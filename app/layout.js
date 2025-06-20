0bc4b01dc6e88d8a1fdb672a616fff32ec49b18e
MD Danish <82753592+Md-Danish-au45@users.noreply.github.com>
2025-06-20 22:00:54 +0530

    update

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,18 @@
-import './globals.css'
+// app/layout.js
+import "./globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
-  title: 'Tutorswala - Quality Tutoring',
-  description: 'Connecting students with expert tutors',
-}
+  title: "Tutorswala - Quality Tutoring",
+  description: "Connecting students with expert tutors",
+};
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
